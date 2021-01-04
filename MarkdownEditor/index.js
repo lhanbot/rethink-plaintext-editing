02452a25032ea33bba26814ad29a83fbb3853d42
lhan <lhan@tableau.com>
2021-01-03 21:09:38 -0800

    implement mark down

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 
 function MarkdownEditor({ file, write }) {
   console.log(file, write);
+
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      setValue(await file.text());
+    })();
+  }, [file]);
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+      <ReactMarkdown plugins={[[gfm, {singleTilde: false}]]}>
+        {value}
+      </ReactMarkdown>
     </div>
   );
 }
